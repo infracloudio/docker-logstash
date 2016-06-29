2449b18c93db8f71a8f6dc19686e7263f1b94465
Vishal <vishal-biyani@users.noreply.github.com>
2016-06-29 17:31:01 +0530

    Missed a $ or two :)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -ri 's/ES_HOST_NAME/'"ES_HOST_NAME"'/g' /logstash/config/logstash.conf
+sed -ri 's/ES_HOST_NAME/'"$ES_HOST_NAME"'/g' /logstash/config/logstash.conf
 /logstash/bin/logstash -f /logstash/config/logstash.conf -v
 
