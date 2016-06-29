0edaa546c2ca30b040ea9dc7f24476974f4a868a
Vishal <vishal-biyani@users.noreply.github.com>
2016-06-29 17:26:37 +0530

    Fixing hostname replacement issue

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "s/^ES_HOST_NAME:.*$/ES_HOST_NAME: ${ES_HOST_NAME}" /logstash/config/logstash.conf
+sed -ri 's/ES_HOST_NAME/'"ES_HOST_NAME"'/g' /logstash/config/logstash.conf
 /logstash/bin/logstash -f /logstash/config/logstash.conf -v
 
